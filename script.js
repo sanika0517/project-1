112d5e5af52a5041e2c84f3d5f75bf24425bc503
sanika0517 <sanikarai1704@gmail.com>
2024-07-08 21:10:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function performSearch() {
+    var category = document.querySelector('.search-dropdown').value;
+    var query = document.querySelector('.search-box').value;
+    alert('Category: ' + category + '\nSearch Query: ' + query);
+    // You can implement your search logic here, e.g., redirecting to a search results page
+}
